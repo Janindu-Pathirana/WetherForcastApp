5c901b7d2cb85bfa14760d6f691670a3d445e431
Janindu-Pathirana <78893460+Janindu-Pathirana@users.noreply.github.com>
2023-12-01 18:08:12 +0530

    Initial Commit

---
+++ WetherForcastApp/WetherForcastAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WetherForcastAppApp.swift
+//  WetherForcastApp
+//
+//  Created by Janindu Pathirana on 2023-12-01.
+//
+
+import SwiftUI
+
+@main
+struct WetherForcastAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
